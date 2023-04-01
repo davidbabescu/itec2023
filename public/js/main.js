03b8492810fbd4c44bab787de5b925b2cdfc537a
davidbabescu <david.babesccu@gmail.com>
2023-04-01 14:00:38 +0300

    Final code img generator

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
 function onSubmit(e) {
     e.preventDefault();
 
+    document.querySelector('.msg').textContent = '';
+    document.querySelector('#image').src = '';
+
     const prompt = document.querySelector('#prompt').value; 
     const size = document.querySelector('#size').value; 
     
@@ -34,7 +37,11 @@ async function generateImageRequest(prompt,size){
         }
 
       const data = await response.json();
-      console.log(data);
+      //console.log(data);
+
+    const imageUrl = data.data;
+
+    document.querySelector('#image').src =imageUrl;
 
       removeLoading();
     } catch (error) {
