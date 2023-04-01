aa5ae89bf3e0f71fa4fd7851f1d2364eb1d2ff4b
davidbabescu <david.babesccu@gmail.com>
2023-04-01 05:53:54 +0300

    Frontend setup v2

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+function onSubmit(e) {
+    e.preventDefault();
+
+    const prompt = document.querySelector('#prompt').value; 
+    const size = document.querySelector('#size').value; 
+    
+    if(prompt === '') {
+        alert('Please add some text');
+        return; 
+    }
+
+    console.log('success');
+    generateImageRequest(prompt,size);
+}
+
+async function generateImageRequest(prompt,size){
+    try {
+        showLoading(); 
+
+        const response= await fetch('/openai/generateimage',{
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt,
+                size
+            })
+        });
+
+        if(!response.ok){
+            removeLoading();
+            throw new Error ('That image cold not be generated');
+        }
+
+      const data = await response.json();
+      console.log(data);
+
+      removeLoading();
+    } catch (error) {
+        document.querySelector('.msg').textContent = error;
+    }
+}
+
+function showLoading(){
+    document.querySelector('.loading').classList.add('show');
+}
+function removeLoading(){
+    document.querySelector('.loading').classList.remove('show');
+}
+
+
+document.querySelector('#image-form').addEventListener('submit', onSubmit );
