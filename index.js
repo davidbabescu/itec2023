2a99a7bc1faa29360e26a2fba28dce0a7fd070a4
davidbabescu <david.babesccu@gmail.com>
2023-04-01 04:41:58 +0300

    Request Body Data Size & Submit

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = process.env.PORT || 5500;
 const app = express();
 const airoutes = require('./routes/openaiRoutes')
 
+//Enable body parser
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/openai', airoutes)
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
